08a4dddec218116ced530dc2833a27ce6d5ede3c
Giulianno Zanetti <giulianno.zanetti@grupomultiplica.com.br>
2022-03-31 14:23:19 -0300

    Primeira Tela Food Health

--- next.config.js
+++ next.config.js
@@ -4,3 +4,22 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+module.exports = {
+  distDir: 'src',
+
+} 
+
+module.exports = {
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+}
+
+
+module.exports = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
